99c15d8a4ffd7908415b21bb7530fe4c3ec8cf61
Divesh Nugessur <Divesh.Nugessur@ceridian.com>
2023-11-08 22:12:47 +0400

    More

---
+++ EmployeeTraining/Interfaces/ITrainingPriority.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeTraining.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTraining.Interfaces
+{
+    internal interface ITrainingPriority
+    {
+         int GetTrainingPriority(User user, Training training);
+    }
+}
---
+++ EmployeeTraining/Services/TrainingPriorityService.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeTraining.Entities;
+using EmployeeTraining.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTraining.Services
+{
+    internal class TrainingPriorityService : ITrainingPriority
+    {
+        public int GetTrainingPriority(User user, Training training)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
