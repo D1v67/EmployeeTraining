642c1372b3d35bd0584775a1db999b66acc3a728
Divesh Nugessur <Divesh.Nugessur@ceridian.com>
2023-11-13 21:30:52 +0400

    Added validation Interface

---
+++ EmployeeTraining/Services/ValidationServices.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeTraining.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTraining.Services
+{
+    public class ValidationServices : IValidationService
+    {
+        public bool IsEmailValid(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsNICValid(string nic)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPasswordValid(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPhoneValid(string phone)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
